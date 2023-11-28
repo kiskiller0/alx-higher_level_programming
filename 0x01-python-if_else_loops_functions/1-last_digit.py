80c354042576afb7102b1d45e21ebf3abbb41257
kiskiller0 <kiskiller0@gmail.com>
2023-11-28 23:57:39 +0100

    test 2

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,7 +2,9 @@
 import random
 number = random.randint(-10000, 10000)
 # YOUR CODE HERE
-remainder = str(number)[-1]
+remainder = int(str(number)[-1])
+
+msg = "sth"
 
 if remainder:
 
@@ -14,4 +16,4 @@ if remainder:
 else:
     msg = "is 0"
 
-print("Last digit of {} is {} and is {}".format(number, remainder, msg)
+print("Last digit of {} is {} and is {}".format(number, remainder, msg))
