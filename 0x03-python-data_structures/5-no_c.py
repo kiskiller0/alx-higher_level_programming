1deb87475ba38516917155e5b0b4f45ca4881a17
kiskiller0 <kiskiller0@gmail.com>
2023-12-04 19:28:47 +0100

    no sea!

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    while 'c' in my_string:
-        my_string.remove('c')
+    return ''.join([c for c in my_string if c != 'c'])
+
+print(no_c("accelerate"))
