a70315d0de6f22c054258706744467d7f1cf1438
kiskiller0 <kiskiller0@gmail.com>
2023-12-04 21:13:30 +0100

    meta info missing

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
+    if not matrix:
+        return None
     for row in range(len(matrix)):
         for col in range(len(matrix[row])):
             print("{:d}".format(matrix[row][col]), end = " " if col != len(matrix[row]) - 1 else "\n")
