61045b6df80ec54bc6a2bace883a183d51094b92
kiskiller0 <kiskiller0@gmail.com>
2023-12-04 21:08:49 +0100

    print matrix

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for row in range(len(matrix)):
+        for col in range(len(matrix[row])):
+            print("{:d}".format(matrix[row][col]), end = " " if col != len(matrix[row]) - 1 else "$\n")
